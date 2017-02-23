d98f022bd73c1f08f9fbb0bbc58d3460e72e930e
Blanca Azucena López <blanca.lopez@data4.mx>
2017-02-23 14:39:10 -0600

    Awesome treemap examples

--- treemap/treemap.controller.js
+++ treemap/treemap.controller.js
@@ -9,12 +9,12 @@
   function TreemapController($timeout) {
     var vm = this;
 
-    var config = {
-      'bindTo': '#chart',
+    var config1 = {
+      'bindTo': '#chart1',
       'size':{
-        'width':960,
-        'height':500,
-        'margin':{top: 20, right: 20, bottom: 30, left: 40},
+        'width':600,
+        'height':600,
+        'margin':{top: 0, right: 0, bottom: 0, left: 0},
       },
       'xAxis':{
         'enabled' : false
@@ -24,17 +24,59 @@
       }
     }
 
-    var chart = dbox.chart(config)
-                  .data({'tsv':'app/d4pkg-dbox/data/data.tsv'})
-                .layer(dbox.treemap)
-                  .nestBy('species')
-                  .size('sepalLength')
-                .end()
-                  .draw();
-
-
-
-
+    var chart1 = dbox.chart(config1)
+        .data({'tsv':'app/d4pkg-dbox/data/test.tsv'})
+      .layer(dbox.treemap)
+        .nestBy(['variable','category','subcategory'])
+        .size('number')
+      .end()
+      .draw();
+      
+    var config2 = {
+      'bindTo': '#chart2',
+      'size':{
+        'width':600,
+        'height':600,
+        'margin':{top: 0, right: 0, bottom: 0, left: 0},
+      },
+      'xAxis':{
+        'enabled' : false
+      },
+      'yAxis': {
+        'enabled' : false
+      }
+    }
+      
+    var chart2 = dbox.chart(config2)
+        .data({'tsv':'app/d4pkg-dbox/data/test.tsv'})
+      .layer(dbox.treemap)
+        .nestBy(['variable','category'])
+        .size('number')
+      .end()
+      .draw();
+      
+    var config3 = {
+      'bindTo': '#chart3',
+      'size':{
+        'width':600,
+        'height':600,
+        'margin':{top: 0, right: 0, bottom: 0, left: 0},
+      },
+      'xAxis':{
+        'enabled' : false
+      },
+      'yAxis': {
+        'enabled' : false
+      }
+    }
+    
+    var chart3 = dbox.chart(config3)
+        .data({'tsv':'app/d4pkg-dbox/data/test.tsv'})
+      .layer(dbox.treemap)
+        .nestBy(['variable'])
+        .size('number')
+      .end()
+      .draw();
   }
 })();
 
