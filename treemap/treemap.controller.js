74c1c373c373c4cf061708017d8dcaf7f41a2f85
Blanca Azucena López <blanca.lopez@data4.mx>
2017-03-03 14:40:17 -0600

    Adding treemap examples

--- treemap/treemap.controller.js
+++ treemap/treemap.controller.js
@@ -8,50 +8,26 @@
   /** @ngInject */
   function TreemapController($timeout) {
     var vm = this;
-    
-    var colors = ['#562B4F','#742C6A','#B23733','#DC8933'];
 
-    var config = {
-      'size':{
-        'width':600,
-        'height':600,
-        'margin':{top: 0, right: 0, bottom: 0, left: 0},
-      },
-      'xAxis':{
-        'enabled' : false
-      },
-      'yAxis': {
-        'enabled' : false
-      }
-    };
+    d3.select("#code").select("code")
+      .each(function(d) {
+        var code = d3.select(this);
+          d3.text("/app/pkg-dbox-examples/treemap/treemap.code.html", function(error, content) {
+            if (error) content = "Sorry, an error occurred.";
+            code.text(content);
+            hljs.highlightBlock(code.node());
+          });
+        });
 
-    dbox.chart(config)
-          .bindTo('#chart1')
-          .data({'tsv':'app/pkg-dbox-examples/data/test.tsv'})
-        .layer(dbox.treemap)
-          .nestBy(['variable','category','subcategory'])
-          .size('number')
-        .end()
-          .draw();
-      
-    dbox.chart(config)
-        .bindTo('#chart2')
-        .data({'tsv':'app/pkg-dbox-examples/data/test.tsv'})
-      .layer(dbox.treemap)
-        .nestBy(['variable','category'])
-        .size('number')
-        .colorScale(colors)
-      .end()
-        .draw();
-    
-    dbox.chart(config)
-        .bindTo('#chart3')
-        .data({'tsv':'app/pkg-dbox-examples/data/test.tsv'})
-    .layer(dbox.treemap)
-      .nestBy(['variable'])
-      .size('number')
-    .end()
-      .draw();
+    d3.select("#data").select("code")
+      .each(function(d) {
+        var code = d3.select(this);
+          d3.text("/app/pkg-dbox-examples/data/test.tsv", function(error, content) {
+            if (error) content = "Sorry, an error occurred.";
+            code.html(content);
+          });
+        });
+
   }
 })();
 
